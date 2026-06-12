b43b06bc7cf98c17adeae96387d0573589f99052
yoann goudin <yoanngoudin@yahoo.fr>
2026-06-12 16:11:25 +0200

    parsing_v2

--- script.py
+++ script.py
@@ -4,8 +4,6 @@ import re
 
 
 filename = "lexique_avec_POS_nettoye.csv"  # File name
-fields = []  # Column names
-rows = []    # Data rows
 dico = {}
 
 with open(filename, 'r') as csvfile:
@@ -14,31 +12,35 @@ with open(filename, 'r') as csvfile:
     fields = next(csvreader)  # Read header
     for row in csvreader:     # Read rows
         dico[row[0]]=row[1]
-        rows.append(row)
 
-    print("Total no. of rows: %d" % csvreader.line_num)  # Row count
 
-print('Field names are: ' + ', '.join(fields))
-
-print('\nFirst 5 rows are:\n')
-for row in rows[:5]:
-    for col in row:
-        print("%10s" % col, end=" ")
-    print('\n')
 print(dico)
 for line in fileinput.input():
     line.rstrip()
     line=line.replace("’","’ ")
-    res=re.split(r"(\s+)",line)
-    #print(res)
+    line=line.replace("."," .")
+    line=line.replace(","," ,")
+    res=re.split(r"\s+",line)
     token_prec = ""
+    etat=0
+    chunk=""
     for token in res:
-        token_min=token
-        if token_prec in ["","!","?","."]:
-            token_min=token.lower()
+        token_min=token.strip("“")
+        if token_prec in ["","!","?",".",":",","]:
+            token_min=token_min.lower()
         pos=dico.get(token_min,0)
         if pos !=0:
-            print(token,pos)
-        token_prec=token
+            #print(token,pos)
+            if etat==0:
+                print(chunk,"\n")
+                chunk=token
+            if etat==1:
+                chunk+=" "+token
+            etat=1
+        else:
+            chunk+=" "+token
+            etat=0
+        #print(token,etat)
+        token_prec=token_min
     #print(line)
 
