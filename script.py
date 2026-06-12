b9b3c46f3a34be0138003046cb48fa5e044b1655
yoann goudin <yoanngoudin@yahoo.fr>
2026-06-12 15:12:14 +0200

    parsing_v1

--- script.py
+++ script.py
@@ -1,13 +1,19 @@
 import csv
-filename = "lexique.csv"  # File name
+import fileinput
+import re
+
+
+filename = "lexique_avec_POS_nettoye.csv"  # File name
 fields = []  # Column names
 rows = []    # Data rows
+dico = {}
 
 with open(filename, 'r') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=';')  # Reader object
 
     fields = next(csvreader)  # Read header
     for row in csvreader:     # Read rows
+        dico[row[0]]=row[1]
         rows.append(row)
 
     print("Total no. of rows: %d" % csvreader.line_num)  # Row count
@@ -19,3 +25,20 @@ for row in rows[:5]:
     for col in row:
         print("%10s" % col, end=" ")
     print('\n')
+print(dico)
+for line in fileinput.input():
+    line.rstrip()
+    line=line.replace("’","’ ")
+    res=re.split(r"(\s+)",line)
+    #print(res)
+    token_prec = ""
+    for token in res:
+        token_min=token
+        if token_prec in ["","!","?","."]:
+            token_min=token.lower()
+        pos=dico.get(token_min,0)
+        if pos !=0:
+            print(token,pos)
+        token_prec=token
+    #print(line)
+
